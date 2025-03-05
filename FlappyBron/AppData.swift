ccd1f89f6e651ffda270be1ddee116818f4dc77e
seanmccain08 <seanmccain327@gmail.com>
2025-03-05 08:10:42 -0600

    Added in ranked icons anc modified ranked interface. Begun implementing rank system - still coming up with the algorithm

---
+++ FlappyBron/AppData.swift
@@ -0,0 +1,32 @@
+//
+//  AppData.swift
+//  FlappyBron
+//
+//  Created by SEAN MCCAIN on 3/5/25.
+//
+
+import Foundation
+
+class AppData{
+    
+    var games = 0
+    var RP = 0
+    
+    /*
+     
+     --- FOR RANKED ---
+     
+     LEBRONZE   - 1,000 RP
+     LESILVER   - 2,000 RP
+     LEGOLD     - 5,000 RP
+     LEDIAMOND  - 10,500 RP
+     LEBRON     - 15,000 RP
+     GOAT       - 25,000 RP
+     
+     --- RANKING ALGORITHM ---
+     
+     
+     
+     */
+    
+}
