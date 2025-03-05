d378c3088368b0dc3c1d39a4a88081e28619d583
Ryan Spencer <ryan.spencer26@d155.org>
2025-03-05 07:46:55 -0600

    started powerup struct

---
+++ FlappyBron/Powerup.swift
@@ -0,0 +1,19 @@
+//
+//  Powerup.swift
+//  FlappyBron
+//
+//  Created by RYAN SPENCER on 3/4/25.
+//
+
+import Foundation
+
+struct Powerup {
+    
+    var buff: Bool
+    var debuff: Bool
+    var deltaSpeed: Int
+    var deltaJump: Int
+    var inputDelay: Int
+    var position: (CGFloat, CGFloat)
+    
+}
