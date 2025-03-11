9818086e372d994b7d8af94f665984cc6e9377a3
seanmccain08 <seanmccain327@gmail.com>
2025-03-11 08:09:32 -0500

    began rank system integration

--- FlappyBron/RankedViewController.swift
+++ FlappyBron/RankedViewController.swift
@@ -10,11 +10,29 @@ import UIKit
 class RankedViewController: UIViewController {
 
     @IBOutlet weak var progressView: UIProgressView!
+    @IBOutlet weak var RPProgressLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        progressView.setProgress(0.75, animated: true)
+        
+        AppData.player?.RP = 1500
+        AppData.player?.rank = 1
+        
+        if(AppData.player?.rank == 0){
+            
+            progressView.setProgress(Float(AppData.player!.RP)/1000.0, animated: true)
+            
+            RPProgressLabel.text = "\(1000-AppData.player!.RP)"
+            
+        }
+        else if(AppData.player?.rank == 1){
+            
+            progressView.setProgress(Float(AppData.player!.RP-1000)/2000.0, animated: true)
+            
+            RPProgressLabel.text = "\(3000-AppData.player!.RP)"
+            
+        }
+        
         
     }
     
