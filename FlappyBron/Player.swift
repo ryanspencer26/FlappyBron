611cca0766d645c2177cc759028e65273486eecb
seanmccain08 <seanmccain327@gmail.com>
2025-03-12 08:02:11 -0500

    did math for ranked in vc

--- FlappyBron/Player.swift
+++ FlappyBron/Player.swift
@@ -20,21 +20,8 @@ class Player : Codable{
         self.username = username
         highScore = 0
         RP = 0
-        if RP >= 1000 {
-            rank = 1
-        } else if RP >= 3000 {
-            rank = 2
-        } else if RP >= 6000 {
-            rank = 3
-        } else if RP >= 10000 {
-            rank = 4
-        } else if RP >= 15000 {
-            rank = 5
-        } else if RP >= 25000 {
-            rank = 6
-        } else {
-            rank = 0
-        }
+        rank = 0
+
     }
     
     public static func save(){
--- FlappyBron/RankedViewController.swift
+++ FlappyBron/RankedViewController.swift
@@ -8,28 +8,115 @@
 import UIKit
 
 class RankedViewController: UIViewController {
+    
+    //        if RP >= 1000 {
+    //            rank = 1 BRONZE
+    //        } else if RP >= 3000 {
+    //            rank = 2 SILVER
+    //        } else if RP >= 6000 {
+    //            rank = 3 GOLD
+    //        } else if RP >= 10000 {
+    //            rank = 4 DIAMOND
+    //        } else if RP >= 15000 {
+    //            rank = 5 LEBRON
+    //        } else if RP >= 25000 {
+    //            rank = 6 GOAT
+    //        } else {
+    //            rank = 0
+    //        }
 
+    @IBOutlet weak var RPdescriptorLabel: UILabel!
     @IBOutlet weak var progressView: UIProgressView!
     @IBOutlet weak var RPProgressLabel: UILabel!
+    @IBOutlet weak var rankImage: UIImageView!
+    @IBOutlet weak var rankLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        AppData.player?.RP = 1500
-        AppData.player?.rank = 1
+        AppData.player!.RP = 52180
         
-        if(AppData.player?.rank == 0){
+        if(AppData.player!.RP < 1000){
+            
+            rankImage.image = UIImage(named: "unranked_icon_ranked")
+            rankLabel.text = "UNRANKED"
             
             progressView.setProgress(Float(AppData.player!.RP)/1000.0, animated: true)
             
             RPProgressLabel.text = "\(1000-AppData.player!.RP)"
+            RPdescriptorLabel.text = "RP TO NEXT RANK"
+
             
         }
-        else if(AppData.player?.rank == 1){
+        else if(AppData.player!.RP < 3000){
             
             progressView.setProgress(Float(AppData.player!.RP-1000)/2000.0, animated: true)
             
             RPProgressLabel.text = "\(3000-AppData.player!.RP)"
+            
+            rankImage.image = UIImage(named: "lebronze_icon_ranked")
+            rankLabel.text = "LEBRONZE"
+            RPdescriptorLabel.text = "RP TO NEXT RANK"
+
+            
+        }
+        else if(AppData.player!.RP < 6000){
+            
+            progressView.setProgress(Float(AppData.player!.RP-3000)/3000.0, animated: true)
+            
+            RPProgressLabel.text = "\(6000-AppData.player!.RP)"
+            
+            rankImage.image = UIImage(named: "lesilver_icon_ranked")
+            rankLabel.text = "LESILVER"
+            RPdescriptorLabel.text = "RP TO NEXT RANK"
+
+            
+        }
+        else if(AppData.player!.RP < 10000){
+            
+            progressView.setProgress(Float(AppData.player!.RP-6000)/4000.0, animated: true)
+            
+            RPProgressLabel.text = "\(10000-AppData.player!.RP)"
+            
+            rankImage.image = UIImage(named: "legold_icon_ranked")
+            rankLabel.text = "LEGOLD"
+            RPdescriptorLabel.text = "RP TO NEXT RANK"
+
+            
+        }
+        else if(AppData.player!.RP < 15000){
+            
+            progressView.setProgress(Float(AppData.player!.RP-10000)/5000.0, animated: true)
+            
+            RPProgressLabel.text = "\(15000-AppData.player!.RP)"
+            
+            rankImage.image = UIImage(named: "lediamond_icon_ranked")
+            rankLabel.text = "LEDIAMOND"
+            RPdescriptorLabel.text = "RP TO NEXT RANK"
+
+            
+        }
+        else if(AppData.player!.RP < 25000){
+            
+            progressView.setProgress(Float(AppData.player!.RP-15000)/10000.0, animated: true)
+            
+            RPProgressLabel.text = "\(25000-AppData.player!.RP)"
+            
+            rankImage.image = UIImage(named: "lebron_icon_ranked")
+            rankLabel.text = "LEBRON"
+            RPdescriptorLabel.text = "RP TO NEXT RANK"
+            
+        }
+        else{
+            
+            progressView.isHidden = true
+            
+            RPProgressLabel.text = "\(AppData.player!.RP)"
+            
+            rankImage.image = UIImage(named: "goat_icon_ranked")
+            rankLabel.text = "GOAT"
+            
+            RPdescriptorLabel.text = "CURRENT RP"
             
         }
         
